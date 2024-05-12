df2af65edb0345a16b53ab3ac4ceb1121faa00f7
Chukobyte <michaelsmith8293@gmail.com>
2024-05-12 15:08:16 -0400

    Removing unused file.

--- src/main.zig
+++ src/main.zig
@@ -2,7 +2,6 @@ const std = @import("std");
 
 const zeika = @import("zeika");
 
-const ecs = @import("engine/asset.zig");
 const data_db = @import("engine/object_data_db.zig");
 const game = @import("game/game.zig");
 
@@ -19,34 +18,31 @@ const GameObject = game.GameObject;
 const Sprite = game.Sprite;
 
 pub fn main() !void {
-    ecs.ECS(.{ Texture });
-
     try zeika.initAll("Zig Test", 800, 600, 800, 600);
 
     const texture_handle: Texture.Handle = Texture.initSolidColoredTexture(1, 1, 255);
     defer Texture.deinit(texture_handle);
 
-    const game_object = GameObject{
-        .transform = Transform2D{ .position = Vec2{ .x = 100.0, .y = 100.0 } },
-        .sprite = Sprite{
-            .texture = texture_handle,
-            .size = Vec2{ .x = 64.0, .y = 64.0 },
-            .draw_source = Rect2{ .x = 0.0, .y = 0.0, .w = 1.0, .h = 1.0 },
-            .modulate = Color.Red,
+    const game_objects = [_]GameObject{
+        GameObject{
+            .transform = Transform2D{ .position = Vec2{ .x = 100.0, .y = 100.0 } },
+            .sprite = Sprite{
+                .texture = texture_handle,
+                .size = Vec2{ .x = 64.0, .y = 64.0 },
+                .draw_source = Rect2{ .x = 0.0, .y = 0.0, .w = 1.0, .h = 1.0 },
+                .modulate = Color.Red,
+            },
+        },
+        GameObject{
+            .transform = Transform2D{ .position = Vec2{ .x = 200.0, .y = 100.0 } },
+            .sprite = Sprite{
+                .texture = texture_handle,
+                .size = Vec2{ .x = 64.0, .y = 64.0 },
+                .draw_source = Rect2{ .x = 0.0, .y = 0.0, .w = 1.0, .h = 1.0 },
+                .modulate = Color.Blue,
+            },
         },
     };
-
-    const game_object2 = GameObject{
-        .transform = Transform2D{ .position = Vec2{ .x = 200.0, .y = 100.0 } },
-        .sprite = Sprite{
-            .texture = texture_handle,
-            .size = Vec2{ .x = 64.0, .y = 64.0 },
-            .draw_source = Rect2{ .x = 0.0, .y = 0.0, .w = 1.0, .h = 1.0 },
-            .modulate = Color.Blue,
-        },
-    };
-
-    const game_objects = [_]GameObject{ game_object, game_object2 };
 
     while (zeika.isRunning()) {
         zeika.update();
