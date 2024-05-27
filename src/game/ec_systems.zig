ff4e243b314ae86f9368c8d07ef59745cb3e059b
Chukobyte <michaelsmith8293@gmail.com>
2024-05-27 11:56:15 -0400

    Updates.

--- src/game/ec_systems.zig
+++ src/game/ec_systems.zig
@@ -6,6 +6,7 @@ const comps = @import("components.zig");
 const Renderer = zeika.Renderer;
 
 const ECSContext = game.ECSContext;
+const ComponentIterator = game.ECSContext.ArchetypeComponentIterator;
 
 const TransformComponent = comps.TransformComponent;
 const SpriteComponent = comps.SpriteComponent;
@@ -23,21 +24,38 @@ pub const MainSystem = struct {
 pub const SpriteRenderingSystem = struct {
     pub fn render(self: *@This(), context: *ECSContext) void {
         _ = self;
-        for (0..context.entity_data_list.items.len) |entity| {
-            if (context.getComponent(entity, TransformComponent)) |transform_comp| {
-                if (context.getComponent(entity, SpriteComponent)) |sprite_comp| {
-                    const draw_config = sprite_comp.sprite.getDrawConfig(&transform_comp.transform, 0);
-                    Renderer.queueDrawSprite(&draw_config);
-                }
-            }
+        var comp_iter = ComponentIterator(&.{ TransformComponent, SpriteComponent }).init(context);
+        while (comp_iter.next()) |node| {
+            const transform_comp = node.getValue(0);
+            const sprite_comp = node.getValue(1);
+            const draw_config = sprite_comp.sprite.getDrawConfig(&transform_comp.transform, 0);
+            Renderer.queueDrawSprite(&draw_config);
         }
+        // for (0..context.entity_data_list.items.len) |entity| {
+        //     if (context.getComponent(entity, TransformComponent)) |transform_comp| {
+        //         if (context.getComponent(entity, SpriteComponent)) |sprite_comp| {
+        //             const draw_config = sprite_comp.sprite.getDrawConfig(&transform_comp.transform, 0);
+        //             Renderer.queueDrawSprite(&draw_config);
+        //         }
+        //     }
+        // }
     }
-    pub fn getComponentTypes() []const type { return &.{ TransformComponent, SpriteComponent }; }
+    pub fn getArchetype() []const type { return &.{ TransformComponent, SpriteComponent }; }
 };
 
 pub const TextRenderingSystem = struct {
     pub fn render(self: *@This(), context: *ECSContext) void {
         _ = self;
+        // var comp_iter = ComponentIterator(&.{ TransformComponent, TextLabelComponent }).init(context);
+        // while (comp_iter.next()) |node| {
+        //     const transform_comp = node.getValue(0);
+        //     const text_label_comp = node.getValue(1);
+        //     if (!text_label_comp.text_label.text.isEmpty()) {
+        //         const draw_config = text_label_comp.text_label.getDrawConfig(transform_comp.transform.position, 0);
+        //         Renderer.queueDrawText(&draw_config);
+        //     }
+        // }
+
         for (0..context.entity_data_list.items.len) |entity| {
             if (context.getComponent(entity, TransformComponent)) |transform_comp| {
                 if (context.getComponent(entity, TextLabelComponent)) |text_label_comp| {
@@ -49,5 +67,5 @@ pub const TextRenderingSystem = struct {
             }
         }
     }
-    pub fn getComponentTypes() []const type { return &.{ TransformComponent, TextLabelComponent }; }
+    pub fn getArchetype() []const type { return &.{ TransformComponent, TextLabelComponent }; }
 };
